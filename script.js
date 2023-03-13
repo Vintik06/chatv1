52ad70dd16166efc918dd6c2c317a9f4093f9a00
Vintik06 <oven.oleinik@gmail.com>
2023-03-13 18:23:10 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+
+// let div = document.getElementById("divs");
+// for (let index = 0; index < 2; index++) {
+//   if (index > 0) {
+//     let para = document.createElement("p");
+//     para.innerText = index;
+//     divs.appendChild(para);
+//     para.addEventListener("click", (e) => {
+//     let ids = para.id = "idp";
+//       console.log(e.target.id(ids));
+//   })
+//   }
+// }
